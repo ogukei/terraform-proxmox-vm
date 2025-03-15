a882a82b22f58a2b9cc59ffc7fb076c86caa5cc6
Keitaro Oguri <ogukei256@gmail.com>
2025-03-16 03:45:13 +0900

    Disable logging

--- main.tf
+++ main.tf
@@ -24,13 +24,6 @@ provider "proxmox" {
   pm_api_token_id = var.pm_api_token_id
   pm_api_token_secret = var.pm_api_token_secret
   pm_tls_insecure = true
-  pm_log_enable = true
-  pm_log_file   = "terraform-plugin-proxmox.log"
-  pm_debug      = true
-  pm_log_levels = {
-    _default    = "debug"
-    _capturelog = ""
-  }
 }
 
 resource "proxmox_vm_qemu" "cloudinit-test" {
